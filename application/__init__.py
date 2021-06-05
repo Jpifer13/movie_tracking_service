d4e9c6fc0ce9686d48813b2052141db3a1fbd882
Jpifer13 <jakepifer3@gmail.com>
2021-06-04 23:00:18 -0400

    creating a branch for next set of updates mainly small readability things

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-# from flask_marshmallow import Marshmallow
 from config import config
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
@@ -8,6 +7,5 @@ flask_app = Flask(__name__)
 flask_app.config.from_object(config)
 CORS(flask_app)
 db = SQLAlchemy(flask_app)
-# marshmallow_app = Marshmallow(flask_app)
 
 from application import routes, models
